aa5c287a5624f06edac0fa016f2244caadfb9899
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2023-01-23 11:45:45 -0800

    tech-debt: adds endpoint to config file (#61)

--- src/components/RenderForm.jsx
+++ src/components/RenderForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import urlBuilder from '../utilities/utils';
 import DataTable from './DataTable/DataTable';
+import CO_PROXY from '../constants/config';
 
 function RenderForm({ typeDropdown, querySelect }) {
   /**
@@ -10,14 +11,12 @@ function RenderForm({ typeDropdown, querySelect }) {
    * @return {React.ReactComponentElement} Table header and rows
    */
 
-  const urlProxy = 'http://localhost:8080/data_assets?';
-
   const [tableData, setTableData] = useState();
   const [message, setMessage] = useState(null);
 
   useEffect(() => {
     if (typeDropdown || querySelect) {
-      const url = urlBuilder(urlProxy, typeDropdown, querySelect);
+      const url = urlBuilder(CO_PROXY, typeDropdown, querySelect);
       const getResponse = async () => {
         const response = await fetch(url).catch((error) => {
           if (!error.response) {
---
+++ src/constants/config.js
@@ -0,0 +1,4 @@
+// RenderForm.jsx
+const CO_PROXY = 'http://localhost:8080/data_assets?';
+
+export default CO_PROXY;
